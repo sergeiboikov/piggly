ea8f3a00dc9da58946650fbb17d34eb9ae4fa56e
Sergei Boikov <sergey.boykov@reksoft.com>
2026-01-29 14:54:33 +0400

    Deleted tests

--- tests/test_encoding_in_docker.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Script to test the encoding bug in a Docker container
-
-# Build a temporary Docker image with the piggly_project code
-docker build -f Dockerfile.test -t piggly-encoding-test ..
-
-# Run the test script
-MSYS_NO_PATHCONV=1 docker run --rm piggly-encoding-test ruby /app/tests/test_encoding_bug.rb
-
